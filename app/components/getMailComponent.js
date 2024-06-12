6ee9466ba79dcfde1162e0fceee2a8cb1f333f32
Iheanacho Ebere <eiheanacho52@gmail.com>
2024-06-13 00:23:02 +0100

    Update
    - Integrated numbering input

--- app/components/getMailComponent.js
+++ app/components/getMailComponent.js
@@ -3,7 +3,7 @@
 import React, { useEffect, useState } from 'react';
 import Loading from '../loading';
 import { useSession } from 'next-auth/react';
-import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure } from "@nextui-org/react";
+import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure, Input } from "@nextui-org/react";
 import dompurify from 'dompurify';
 
 function GetMailComponent() {
@@ -13,7 +13,7 @@ function GetMailComponent() {
   const [error, setError] = useState(null);
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
   const [selectedMail, setSelectedMail] = useState(null);
-  console.log(session)
+  const [maxNumberOfMails, setMaxNumberOfMails] = useState(5)
 
   const fetchData = async () => {
     try {
@@ -23,7 +23,7 @@ function GetMailComponent() {
       const openAiKey = process.env.NEXT_PUBLIC_OPENAI_API_KEY;
       const googleAccessToken = localStorage.getItem("accessToken");
 
-      const res = await fetch('https://gmailloader.onrender.com/mail', {
+      const res = await fetch(`https://gmailloader.onrender.com/mail/${maxNumberOfMails}`, {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
@@ -102,7 +102,24 @@ function GetMailComponent() {
 
   return (
     <div className="w-full h-full flex flex-col justify-center items-center gap-2">
-      <div className="mx-auto w-[70%] flex justify-end">
+      <div className="mx-auto w-[70%] flex justify-between">
+        <div className='flex gap-2'>
+          <Input 
+            value={maxNumberOfMails}
+            onChange={(e)=>{
+              e.preventDefault()
+              setMaxNumberOfMails(e.target.value)
+            } }
+            label="Number of mail"
+            size='sm'
+            radius='sm'
+          />
+          <Button 
+          disabled={maxNumberOfMails < 0 }
+          onClick={fetchData}
+          className='text-white text-center bg-gray-900 text-sm px-5 py-2.5 inline-flex items-center me-2 mb-2 w-full h-full'
+          size='sm'> Refresh</Button>
+        </div>
         <p className=" font-light text-xl text-gray-900 hover:cursor-pointer hover:underline" onClick={classifyData}>Classify</p>
       </div>
       <div className="mx-auto max-w-[75%] lg:max-w-[60%]  space-y-4">
